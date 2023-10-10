2d88b5269c7a67e1913694f8db53f6e2fad187ab
rivera <billy100007@gmail.com>
2023-10-10 09:18:02 +0800

    1st Commit

---
+++ pages/register/code.php
@@ -0,0 +1,29 @@
+<?php
+session_start();        
+require 'config.php';
+
+if(isset($_POST['save_student']))
+{
+    $email = mysqli_real_escape_string($con,$_POST['email']);
+    $firstName = mysqli_real_escape_string($con,$_POST['firstName']);
+    $middleName = mysqli_real_escape_string($con,$_POST['middleName']);
+    $lastName = mysqli_real_escape_string($con,$_POST['lastName']);
+    $password = mysqli_real_escape_string($con,$_POST['password']);
+    
+    
+
+    //$query = "INSERT INTO user (uemail, ufname, umname, ulname, upassword) VALUES ('$email','$firstName','$middleName','$lastName' ,'$password')";
+    $query = "INSERT INTO user (email, firstName, lastName, password) VALUES ('$email','$firstName','$lastName' ,'$password')";
+    $query_run = mysqli_query($con, $query);
+    if($query_run){
+        $_SESSION['message'] = "Student Created Successfully";
+        header("Location: register.php");
+        exit(0);
+    }else{
+        $_SESSION['message'] = "Student NOT CREATED";
+        header("Location: register.php");
+         exit(0);
+    }
+
+}
+?>
