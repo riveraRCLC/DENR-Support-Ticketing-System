ea9e05de3e582f9c81cdac8dfdc07f17172f6e91
rivera <billy100007@gmail.com>
2023-10-10 10:37:57 +0800

    dashboard is viewable

--- pages/Dashboard/dashboard.php
+++ pages/Dashboard/dashboard.php
@@ -8,15 +8,15 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="/DENR-Support-Ticketing-System/plugins/fontawesome-free/css/all.min.css">
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" href="/DENR-Support-Ticketing-System/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
   <!-- Tempusdominus Bootstrap 4 -->
-  <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <link rel="stylesheet" href="/DENR-Support-Ticketing-System/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="/DENR-Support-Ticketing-System/dist/css/adminlte.min.css">
 
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
@@ -304,24 +304,24 @@
 
 
 <!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
+<script src="/DENR-Support-Ticketing-System/plugins/jquery/jquery.min.js"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="plugins/jquery-ui/jquery-ui.min.js"></script>
+<script src="/DENR-Support-Ticketing-System/plugins/jquery-ui/jquery-ui.min.js"></script>
 
 
 
 <!-- Bootstrap 4 -->
 <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<script src="/DENR-Support-Ticketing-System/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
 <!-- overlayScrollbars -->
-<script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<script src="/DENR-Support-Ticketing-System/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 
 
 
 
 <!-- AdminLTE App -->
-<script src="dist/js/adminlte.js"></script>
+<script src="/DENR-Support-Ticketing-System/dist/js/adminlte.js"></script>
 
 </body>
 </html>
