996794ce7ea091169d559d011f96e0e0f3811f04
rivera <billy100007@gmail.com>
2023-10-11 13:41:04 +0800

    Dashboard 1st Commit

--- pages/Dashboard/dashboard.php
+++ pages/Dashboard/dashboard.php
@@ -107,19 +107,19 @@
                 </a>
                 <ul class="nav nav-treeview">
                   <li class="nav-item">
-                    <a href="pages/mailbox/mailbox.html" class="nav-link">
+                    <a href="/DENR-Support-Ticketing-System/pages/mailbox/mailbox.html" class="nav-link">
                       <i class="far fa-circle nav-icon"></i>
                       <p>Inbox</p>
                     </a>
                   </li>
                   <li class="nav-item">
-                    <a href="pages/mailbox/compose.html" class="nav-link">
+                    <a href="/DENR-Support-Ticketing-System/pages/mailbox/compose.html" class="nav-link">
                       <i class="far fa-circle nav-icon"></i>
                       <p>Compose</p>
                     </a>
                   </li>
                   <li class="nav-item">
-                    <a href="pages/mailbox/read-mail.html" class="nav-link">
+                    <a href="/DENR-Support-Ticketing-System/pages/mailbox/read-mail.html" class="nav-link">
                       <i class="far fa-circle nav-icon"></i>
                       <p>Read</p>
                     </a>
