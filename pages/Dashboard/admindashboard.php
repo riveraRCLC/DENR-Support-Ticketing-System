5546711b8e80a0bd5454af72381a9a3b8467394f
Rivera <billy100007@gmail.com>
2023-11-26 09:38:07 +0800

    logo changes

--- pages/Dashboard/admindashboard.php
+++ pages/Dashboard/admindashboard.php
@@ -49,7 +49,7 @@ if($_SESSION["email"]) {
     <!-- Brand Logo -->
     <a href="../../index3.html" class="brand-link">
       <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">Ticketing System</span>
     </a>
 
      <!-- Sidebar -->
@@ -60,7 +60,8 @@ if($_SESSION["email"]) {
           <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+        <a href="#" class="d-block">Welcome</a> 
+        <a href="#" class="d-block"><?php echo $_SESSION["ufname"] . ' ' . $_SESSION["ulname"]; ?></a>
           
         </div>
      
