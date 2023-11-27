9c4ce6df839781498d948ccecce2b654806f4b62
Rivera <billy100007@gmail.com>
2023-11-27 12:46:27 +0800

    some changes

--- pages/mailbox/TestReadMail.php
+++ pages/mailbox/TestReadMail.php
@@ -6,6 +6,10 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+    <!--<link rel="stylesheet" href="style.css">-->
   <title> Read Mail</title>
 
   <!-- Google Font: Source Sans Pro -->
