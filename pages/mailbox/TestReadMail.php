0f7e33e9af1adbb689c1173033ebfd303480a427
Rivera <billy100007@gmail.com>
2023-11-28 01:21:28 +0800

    some changes and back up

--- pages/mailbox/TestReadMail.php
+++ pages/mailbox/TestReadMail.php
@@ -348,7 +348,7 @@ if($_SESSION["email"]) {
             <!-- /.card-footer -->
             <div class="card-footer">
               <div class="float-right">
-                <button type="button" class="btn btn-default"><i class="fas fa-reply"></i> Reply</button>
+                <button type="button" class="btn btn-default"><i class="fas fa-reply" onclick="ReplyAccordion()"></i> Reply</button>
                 <button type="button" class="btn btn-default"><i class="fas fa-share"></i> Forward</button>
               </div>
               <button type="button" class="btn btn-default"><i class="far fa-trash-alt"></i> Delete</button>
@@ -423,7 +423,39 @@ document.getElementById("TESTINGidREAD").textContent = tempMerge;
                   $('#letterBody').html(tempBody);
                   ReadMailList(tempConvoid, tempTicketID);
               });
-             
+              function ReplyAccordion() {
+                      var tr = '';
+                      tr += '<div class="card-body">';
+                      tr += '<div class="form-group">';
+                      tr += '<input type="email" id="to_input" class="form-control" placeholder="To:">';
+                      tr += '</div>';
+                      tr += '<div class="form-group">';
+                      tr += '<input type="text" id="subject_input" class="form-control" placeholder="Subject:">';
+                      tr += '</div>';
+                      tr += '<div class="form-group">';
+                      tr += '<textarea id="compose_textarea" class="form-control" style="height: 300px"></textarea>';
+                      tr += '</div>';
+                      tr += '<div class="form-group">';
+                      tr += '<div class="btn btn-default btn-file">';
+                      tr += '<i class="fas fa-paperclip"></i> Attachment';
+                      tr += '<input type="file" name="attachment">';
+                      tr += '</div>';
+                      tr += '<p class="help-block">Max. 32MB</p>';
+                      tr += '</div>';
+                      tr += '</div>';
+                      tr += '<!-- /.card-body -->';
+                      tr += '<div class="card-footer">';
+                      tr += '<div class="float-right">';
+                      tr += '<button type="button" class="btn btn-default"><i class="fas fa-pencil-alt"></i> Draft</button>';
+                      tr += '<button type="submit" class="btn btn-primary" onclick="addTicket()"><i class="far fa-envelope"></i> Send</button>';
+                      tr += '</div>';
+                      tr += '<button type="reset" class="btn btn-default"><i class="fas fa-times"></i> Discard</button>';
+                      tr += '</div>';  // This div closes the card-footer
+                      tr += '</div>';  // This div closes the card-outline
+                      tr += '</div>';  // This div closes the card
+
+                          $('#ReadMail_Data').append(tr);  // Append the content
+                      }
               function ReadMailList(tempConvoid, tempTicketID) {
                 $.ajax({
                     type: 'post', // Change to 'post' if you want to use POST method
@@ -521,7 +553,7 @@ document.getElementById("TESTINGidREAD").textContent = tempMerge;
             });   
           }
 
-          function addTicket() {
+          function replyConversation() {
           var conSub = $('#subject_input').val();
           var conSenderID = <?php echo $_SESSION["id"]; ?>;
           var conReceiverID = $('#to_input').val();
