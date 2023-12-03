4c3a81e33b775d8a90f055226ba74521c19a9cc1
Rivera <billy100007@gmail.com>
2023-12-04 02:45:46 +0800

    try hard

---
+++ pages/UserDetails/backup/Part2.php
@@ -0,0 +1,61 @@
+
+<?php
+
+function addUserDetails($conn, $firstName, $middleName, $lastName, $phoneNum, $company)
+{
+    session_start();
+    $userID = $_SESSION["id"];
+
+    // Update user table
+    $updateUserQuery = "UPDATE `user` SET `ufname` = ?, `umname` = ?, `ulname` = ?, `phonenum` = ? WHERE `userid` = ?";
+    $stmt = $conn->prepare($updateUserQuery);
+    $stmt->bind_param("ssssi", $firstName, $middleName, $lastName, $phoneNum, $userID);
+    $stmt->execute();
+    $stmt->close();
+
+    // Check if the user is associated with a company
+    $userCompanyQuery = "SELECT `udcompid` FROM `userdetails` WHERE `uduserid` = ?";
+    $stmt = $conn->prepare($userCompanyQuery);
+    $stmt->bind_param("i", $userID);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+    $stmt->close();
+
+    if ($row['udcompid'] === null) {
+        // User is not associated with any company, insert a new record
+        insertUserDetails($conn, $userID, $company);
+    } else {
+        // User is associated with a company, update the record
+        $companyID = getCompanyIdByName($conn, $company);
+
+        if ($companyID !== null) {
+            // Company exists, update userdetails table
+            $updateUserDetailsQuery = "UPDATE `userdetails` SET `udcompid` = ? WHERE `uduserid` = ?";
+            $stmt = $conn->prepare($updateUserDetailsQuery);
+            $stmt->bind_param("ii", $companyID, $userID);
+            $stmt->execute();
+            $stmt->close();
+        } else {
+            // Company doesn't exist, handle this case (e.g., insert the company first or show an error)
+            // You may want to implement a function to insert a new company
+            // insertNewCompany($conn, $company);
+        }
+    }
+
+    header("Location: /DENR-Support-Ticketing-System/pages/UserDetails/UserDetails.php?success=update");
+    exit();
+}
+
+// Function to insert a new record in userdetails table
+function insertUserDetails($conn, $userID, $company)
+{
+    $companyID = getCompanyIdByName($conn, $company);
+
+    // Insert new record in userdetails table
+    $insertUserDetailsQuery = "INSERT INTO `userdetails` (`uduserid`, `udcompid`) VALUES (?, ?)";
+    $stmt = $conn->prepare($insertUserDetailsQuery);
+    $stmt->bind_param("ii", $userID, $companyID);
+    $stmt->execute();
+    $stmt->close();
+}
--- pages/UserDetails/includes/function.inc.php
+++ pages/UserDetails/includes/function.inc.php
@@ -1,43 +1,90 @@
 <?php
 
-
 function addUserDetails($conn, $firstName, $middleName, $lastName, $phoneNum, $company)
 {
-    session_start();
-    $userID = $_SESSION["id"];
+    try {
+        session_start();
+        $userID = $_SESSION["id"];
 
-    // Update user table
-    $updateUserQuery = "UPDATE `user` SET `ufname` = ?, `umname` = ?, `ulname` = ?, `phonenum` = ? WHERE `userid` = ?";
-    $stmt = $conn->prepare($updateUserQuery);
-    $stmt->bind_param("ssssi", $firstName, $middleName, $lastName, $phoneNum, $userID);
-    $stmt->execute();
-    $stmt->close();
+        // Update user table
+        $updateUserQuery = "UPDATE `user` SET `ufname` = ?, `umname` = ?, `ulname` = ?, `phonenum` = ? WHERE `userid` = ?";
+        $stmt = $conn->prepare($updateUserQuery);
+        $stmt->bind_param("ssssi", $firstName, $middleName, $lastName, $phoneNum, $userID);
+        $stmt->execute();
+        $stmt->close();
 
-    // Retrieve company ID based on company name
-    $companyID = getCompanyIdByName($conn, $company);
+        // Check if the user is associated with a company
+        $userCompanyQuery = "SELECT `udcompid` FROM `userdetails` WHERE `uduserid` = ?";
+        $stmt = $conn->prepare($userCompanyQuery);
+        $stmt->bind_param("i", $userID);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $row = $result->fetch_assoc();
+        $stmt->close();
 
-    // Update userdetails table
-    $updateUserDetailsQuery = "UPDATE `userdetails` SET `udcompid` = ? WHERE `uduserid` = ?";
-    $stmt = $conn->prepare($updateUserDetailsQuery);
-    $stmt->bind_param("ii", $companyID, $userID);
-    $stmt->execute();
-    $stmt->close();
+        if ($row['udcompid'] === null) {
+            // User is not associated with any company, insert a new record
+            insertUserDetails($conn, $userID, $company);
+        } else {
+            // User is associated with a company, update the record
+            $companyID = getCompanyIdByName($conn, $company);
 
-    header("Location: /DENR-Support-Ticketing-System/pages/UserDetails/UserDetails.php?success=update");
-    exit();
+            if ($companyID !== null) {
+                // Company exists, update userdetails table
+                $updateUserDetailsQuery = "UPDATE `userdetails` SET `udcompid` = ? WHERE `uduserid` = ?";
+                $stmt = $conn->prepare($updateUserDetailsQuery);
+                $stmt->bind_param("ii", $companyID, $userID);
+                $stmt->execute();
+                $stmt->close();
+            } else {
+                // Company doesn't exist, handle this case (e.g., insert the company first or show an error)
+                // You may want to implement a function to insert a new company
+                // insertNewCompany($conn, $company);
+            }
+        }
+
+        header("Location: /DENR-Support-Ticketing-System/pages/UserDetails/UserDetails.php?success=update");
+        exit();
+    } catch (Exception $e) {
+        // Handle exceptions here (e.g., log the error, redirect to an error page)
+        echo "An error occurred: " . $e->getMessage();
+    }
 }
 
+// Function to insert a new record in userdetails table
+function insertUserDetails($conn, $userID, $company)
+{
+    try {
+        $companyID = getCompanyIdByName($conn, $company);
+
+        // Insert new record in userdetails table
+        $insertUserDetailsQuery = "INSERT INTO `userdetails` (`uduserid`, `udcompid`) VALUES (?, ?)";
+        $stmt = $conn->prepare($insertUserDetailsQuery);
+        $stmt->bind_param("ii", $userID, $companyID);
+        $stmt->execute();
+        $stmt->close();
+    } catch (Exception $e) {
+        // Handle exceptions here (e.g., log the error, redirect to an error page)
+        echo "An error occurred: " . $e->getMessage();
+    }
+}
 
 function getCompanyIdByName($conn, $companyName)
 {
-    $getCompanyIdQuery = "SELECT `compid` FROM `company` WHERE `compname` = ?";
-    $stmt = $conn->prepare($getCompanyIdQuery);
-    $stmt->bind_param("s", $companyName);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $row = $result->fetch_assoc();
-    $companyID = $row['compid'];
-    $stmt->close();
+    try {
+        $getCompanyIdQuery = "SELECT `compid` FROM `company` WHERE `compname` = ?";
+        $stmt = $conn->prepare($getCompanyIdQuery);
+        $stmt->bind_param("s", $companyName);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $row = $result->fetch_assoc();
+        $companyID = $row['compid'];
+        $stmt->close();
 
-    return $companyID;
+        return $companyID;
+    } catch (Exception $e) {
+        // Handle exceptions here (e.g., log the error, redirect to an error page)
+        echo "An error occurred: " . $e->getMessage();
+        return null;
+    }
 }
